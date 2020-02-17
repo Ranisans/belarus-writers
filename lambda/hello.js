ea502057edf50300336abbb52c0f9232ca62375c
Ranisans <ranisans@yahoo.com>
2020-02-17 16:31:38 +0300

    feat: rename a component from * .js to * .tsx leave a minimum of data in the components

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,14 +1,14 @@
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 module.exports.handler = async function(event, context) {
-  console.log("queryStringParameters", event.queryStringParameters)
+  console.log('queryStringParameters', event.queryStringParameters);
   return {
     // return null to show no errors
     statusCode: 200, // http status code
     body: JSON.stringify({
-      msg: "Hello, World! This is better " + Math.round(Math.random() * 10)
-    })
-  }
-}
+      msg: `Hello, World! This is better ${Math.round(Math.random() * 10)}`,
+    }),
+  };
+};
 
 // Now you are ready to access this API from anywhere in your Gatsby app! For example, in any event handler or lifecycle method, insert:
 // fetch("/.netlify/functions/hello")
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const Index = () => (
+  <Layout>
+    <div>Hello</div>
+  </Layout>
+);
+
+export default Index;
