b509cae446f6ed7f6cc652946f6af5ffd53a473e
Ranisans <ranisans@yahoo.com>
2020-02-17 10:55:51 +0300

    feat: add a declaration for importing images

---
+++ d.ts
@@ -0,0 +1,14 @@
+declare module '*.svg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: any;
+  export default value;
+}
