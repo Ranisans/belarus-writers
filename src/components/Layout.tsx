1b69691e1b9dc067f3f5eb20ffaed1348d0844fa
Anna Kulay <annakulay10@gmail.com>
2020-02-19 22:15:28 +0300

    fix: fix all

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Footer from './Footer';
 import Navbar from './Navbar/Navbar';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
 
 interface PropsType {
   children: React.ReactNode;
@@ -31,15 +29,13 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 const TemplateWrapper = ({ children }: PropsType) => {
   const classes = useStyles();
   return (
-    <div >
-      <div className={classes.root}>
-        <CssBaseline />
-        <div className={classes.content}>
-          <Navbar />
-          <main className={classes.main}>{children}</main>
-        </div>     
-        <Footer />
-      </div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <div className={classes.content}>
+        <Navbar />
+        <main className={classes.main}>{children}</main>
+      </div>     
+      <Footer />
     </div>
   )
 };
